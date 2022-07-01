29a96490592e4e5319d738f1610f72b5c88c4259
innody <innody@shiftup.co.kr>
2022-07-01 12:04:18 +0900

    Add Plugins uniRx Mysql.Data.6.9.9

---
+++ Assets/Scripts/Game/Manager/IManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class IManager<T> : MonoBehaviour where T : Component
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance is null)
+            {
+                _instance = FindObjectOfType<T>();
+                if (_instance is null)
+                {
+                    GameObject newInstance = new GameObject();
+                    _instance = newInstance.AddComponent<T>();
+                    _instance.name = typeof(T).FullName;
+                }
+            }
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        _instance = this as T;
+    }
+}
